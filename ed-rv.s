a12a03f83ed197713431b4e8dc3282728bfce3f5
Simon <simondepomian@gmail.com>
2026-03-21 21:52:46 +0100

    added weclome message

--- ed-rv.s
+++ ed-rv.s
@@ -9,13 +9,18 @@
 .equ S_IRGRP, 00040 #group has read permission
 .equ S_IROTH, 00004 #others have read permission
 
+.text
 .global _start
 
-_start:
-	# initialize global pointer
-	la gp, __global_pointer$ # name defined from default linker script
+main:
+	# print welcome meassage
+	addi a7, zero, 64
+	addi a0, zero, 1 #stdout
+	la a1, welcome
+	addi a2, zero, 19 # bytes
+	ecall
 
-	# open syscall
+	# open syscall, used to open buffer as file.
 	addi a7, zero, 56
 	addi a0, zero, AT_FDCWD # dirfd
 	la a1, hello # path
@@ -33,5 +38,14 @@ _start:
 	# add a0, zero, a0
 	ecall
 
+_start:
+	# initialize global pointer
+	la gp, __global_pointer$ # name defined from default linker script
+	j main
+
+.data
+welcome:
+	.ascii "Welcome to ed-rv!\n\0"
 hello:
 	.ascii "myfile\0"
+
