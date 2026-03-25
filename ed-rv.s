1d4de38018a60e91c15a01e2847c2d296aa06188
Simon <simondepomian@gmail.com>
2026-03-25 20:49:03 +0100

    print multible times using lseek

--- ed-rv.s
+++ ed-rv.s
@@ -3,14 +3,21 @@
 .equ O_RDWR, 2
 .equ O_CREAT, 64
 
+# flag for lseek
+.equ SEEK_SET, 0 # "The file offset is set to offset bytes."
+
 # file permissions
 .equ S_IRUSR, 00400 #user has read permission
 .equ S_IWUSR, 00200 #user has write permission
 .equ S_IRGRP, 00040 #group has read permission
 .equ S_IROTH, 00004 #others have read permission
 
-# s1 will only be used to check for unwritten changes
-#.set unwritten_changes, s1 # WHY NO WORK
+### --- Register "defenitions" --- ###
+# s1 is always used to store the returned file descriptor number.
+# s2 is always used to check for unwritten changes. 1 for true 0 for false.
+#
+# (i wish .set unwritten_changes, s2 would just work instead)
+###
 
 .text
 .global _start
@@ -32,6 +39,7 @@ main:
 	addi t1, zero, 2
 	bltu t0, t1, newfile # if argc < (unsigned) 2: j newfile
 		ld a1, 16(sp) # set filename to argv[1]
+		#TODO make a copy as a file buffer if opening existing file
 		j newfile_end
 	newfile:
 		la a1, buffer_path # path
@@ -43,7 +51,7 @@ main:
 	ecall
 
 	#save returned file descriptor
-	addi s2, a0, 0
+	addi s1, a0, 0
 
 	# main normal_loop
 	normal_loop:
@@ -61,33 +69,40 @@ main:
 		
 		# If print file command
 		bne t0, t2, skip_print
-			### Read 8 bytes from file test
+
+			# lseek syscall to read from begining of file
+			addi a7, zero, 62
+			addi a0, s1, 0 # fd
+			addi a1, zero, 0 # bytes in (beginning)
+			addi a2, zero, SEEK_SET
+			ecall
+
+			### Read from file test
 			addi a7, zero, 63
-			addi a0, s2, 0
-			la	 a1, input_buffer
-			addi a2, zero, 8
+			addi a0, s1, 0
+			la	 a1, read_buffer
+			addi a2, zero, 255
 			ecall
 
 			# print what was read 
 			addi a7, zero, 64
+			#la a1, welco #same buffer still in a1
+			addi a2, a0, 0 # print bytes read
 			addi a0, zero, 1 #stdout
-			#la a1, welco
-			addi a2, zero, 8 # bytes
 			ecall
 		skip_print:
 
 		bne t0, t1, normal_loop # if t0 != 'q': normal_loop
 
 	# Checks for unwritten changes
-	#bnez unwritten_changes, normal_loop # WHY NO WORK
-	beqz s1, break # s1 = unwritten_changes (true/false)
+	beqz s2, break # s2 = unwritten_changes (true/false)
 		#TODO print error meassage here
 		j normal_loop
 	break:
 
 	# close syscall
 	addi a7, zero, 57
-	add a0, s2, 0 # file descriptor returned from open()
+	add a0, s1, 0 # s1 is file descriptor returned from open()
 	ecall
 
 	# exit syscall
@@ -107,3 +122,4 @@ welcome:
 buffer_path: #TODO change to /tmp/ed-rv_buff
 	.ascii "ed-rv_buff\0"
 input_buffer: .space 8
+read_buffer: .space 255
